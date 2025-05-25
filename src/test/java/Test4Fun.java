f49084723dd57e9e77b3d3017a48f3e927383d1e
Shpend95 <shpendpllana5@gmail.com>
2025-05-25 13:21:39 -0400

    3rd commit

--- src/test/java/Test4Fun.java
+++ src/test/java/Test4Fun.java
@@ -3,18 +3,30 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Test4Fun {
     public static void main(String[] args) {
 
         WebDriver driver=new EdgeDriver();
+        driver.manage().window().maximize();
         driver.get("http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/dashboard");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         WebElement username= driver.findElement(By.xpath("//*[@name='txtUsername']"));
         username.sendKeys("Admin");
         WebElement password= driver.findElement(By.xpath("//*[@name='txtPassword']"));
         password.sendKeys("Hum@nhrm123");
         WebElement loginBtn= driver.findElement(By.xpath("//*[@id='btnLogin']"));
         loginBtn.click();
+
+
+
+        WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(10));
+
+        WebElement weclomeMsg=driver.findElement(By.xpath("//body/div[1]/div[1]/a[2]"));
+        weclomeMsg.click();
 
 
     }
